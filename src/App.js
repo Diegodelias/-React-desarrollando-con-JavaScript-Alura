f4bb4fe908b430a2788f51951b663801be9ab7e9
Diegodelias <diegodelias@gmail.com>
2023-04-06 23:09:53 -0300

    clase3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./componentes/Header/Header"
+import Formulario from "./componentes/Formulario/Formulario"
 function App() {
   return (
     <div>
       
       <Header/> 
+      <Formulario/>
     
    
     </div>
