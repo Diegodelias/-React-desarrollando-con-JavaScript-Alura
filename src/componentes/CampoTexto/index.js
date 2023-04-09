2af1312e87e2585a5b39a5ab726392d13c952028
Diegodelias <diegodelias@gmail.com>
2023-04-08 22:26:01 -0300

    fin formulario basico

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,11 +1,20 @@
 import "./CampoTexto.css"
+import {  useState } from "react"
 
 const  CampoTexto = (props) => {
-   console.log("Datos: ",props)
+const [ valor, actualizarValor] = useState("")
+
    const placeholderModificado = `${props.placeholder}...`
+   const manejarCambio = (e)=>{
+    console.log("cambio", e.target.value)
+    props.actualizarValor(e.target.value)
+   }
    return <div className="campo-texto">
         <label>{props.titulo}</label>
-        <input placeholder={placeholderModificado} required={props.required}/>   
+        <input placeholder={placeholderModificado} 
+        required={props.required} 
+        value={props.valor}
+        onChange={manejarCambio}/>   
     </div>
 }
 
